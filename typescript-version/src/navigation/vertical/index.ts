3019586809541e67918bab5a4e0134cefe8ff501
xtan44 <xtanpequiro@gmail.com>
2024-08-07 12:30:03 +0800

    navigations
    nextjs and sidebar update

--- typescript-version/src/navigation/vertical/index.ts
+++ typescript-version/src/navigation/vertical/index.ts
@@ -1,14 +1,9 @@
 // ** Icon imports
-import Login from 'mdi-material-ui/Login'
-import Table from 'mdi-material-ui/Table'
-import CubeOutline from 'mdi-material-ui/CubeOutline'
-import HomeOutline from 'mdi-material-ui/HomeOutline'
-import FormatLetterCase from 'mdi-material-ui/FormatLetterCase'
-import AccountCogOutline from 'mdi-material-ui/AccountCogOutline'
-import CreditCardOutline from 'mdi-material-ui/CreditCardOutline'
-import AccountPlusOutline from 'mdi-material-ui/AccountPlusOutline'
-import AlertCircleOutline from 'mdi-material-ui/AlertCircleOutline'
-import GoogleCirclesExtended from 'mdi-material-ui/GoogleCirclesExtended'
+import ViewDashboardOutline from 'mdi-material-ui/ViewDashboardOutline'
+import AlertOutline from 'mdi-material-ui/AlertOutline'
+import FileOutline from 'mdi-material-ui/FileOutline'
+import FaceAgent from 'mdi-material-ui/FaceAgent'
+import Logout from 'mdi-material-ui/Logout'
 
 // ** Type import
 import { VerticalNavItemsType } from 'src/@core/layouts/types'
@@ -17,63 +12,29 @@ const navigation = (): VerticalNavItemsType => {
   return [
     {
       title: 'Dashboard',
-      icon: HomeOutline,
-      path: '/'
+      icon: ViewDashboardOutline,
+      path: '/admin/orders'
     },
     {
-      title: 'Account Settings',
-      icon: AccountCogOutline,
-      path: '/account-settings'
+      title: 'Alert Management',
+      icon: AlertOutline,
+      path: '/admin/geo/alerts'
     },
     {
-      sectionTitle: 'Pages'
+      title: 'Reports',
+      icon: FileOutline,
+      path: '/admin/geo/reports'
     },
     {
-      title: 'Login',
-      icon: Login,
-      path: '/pages/login',
-      openInNewTab: true
+      title: 'Help and Support',
+      icon: FaceAgent,
+      path: '/admin/geo/support'
     },
     {
-      title: 'Register',
-      icon: AccountPlusOutline,
-      path: '/pages/register',
-      openInNewTab: true
+      title: 'Logout',
+      icon: Logout,
+      path: '/admin/services'
     },
-    {
-      title: 'Error',
-      icon: AlertCircleOutline,
-      path: '/pages/error',
-      openInNewTab: true
-    },
-    {
-      sectionTitle: 'User Interface'
-    },
-    {
-      title: 'Typography',
-      icon: FormatLetterCase,
-      path: '/typography'
-    },
-    {
-      title: 'Icons',
-      path: '/icons',
-      icon: GoogleCirclesExtended
-    },
-    {
-      title: 'Cards',
-      icon: CreditCardOutline,
-      path: '/cards'
-    },
-    {
-      title: 'Tables',
-      icon: Table,
-      path: '/tables'
-    },
-    {
-      icon: CubeOutline,
-      title: 'Form Layouts',
-      path: '/form-layouts'
-    }
   ]
 }
 
